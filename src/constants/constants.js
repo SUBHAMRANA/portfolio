ac29399e9d8fe83a6fb23f05b5fff59fb1503774
SUBHAMRANA <54814938+SUBHAMRANA@users.noreply.github.com>
2024-07-10 22:05:15 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,71 +2,22 @@ export const projects = [
   {
     title: "Chatify",
     description:
-      "Developed a feature-rich chat app with real-time messaging and integrated a webRTC-based video chat, enabling users to join designated rooms for face-to-face communication.",
+      "Developed a feature-rich chat app with real-time messaging and integrated a webRTC-based video chat, enabling users to connect for face-to-face communication.",
     image: "/images/3.png",
-    tags: ["MERN STACK", "ChakraUI"],
-    source: "https://github.com/bhattritik21/Chatify-App",
-    visit: "https://chatify-app-25vx.onrender.com",
+    tags: ["MERN STACK"],
+    source: "https://github.com/SUBHAMRANA/Video_Chat_App",
+    visit: "https://teams-clone-ms-engage.netlify.app/",
     id: 0,
   },
   {
-    title: " Snake Game",
+    title: " RScanner",
     description:
-      "JavaScript Advance Project. Made a snake game using pure JavaScript for the logic and CSS for the animations",
+      "Revolutionizing job applications with MERN Stack and OpenAI! Effortlessly tailor resumes with keyword matching, insights, and suggestions for standout resumes. Transform your application process today.",
     image: "/images/6.png",
-    tags: ["Html", "css", "Vanilla JS"],
+    tags: ["MERN", "OpenAi", "Bootstrap"],
     source: "https://github.com/bhattritik21/snake-game",
-    visit: "https://bhattritik21.github.io/snake-game/",
+    visit: "https://rscanner.onrender.com/",
     id: 1,
-  },
-  {
-    title: "CocktailDb",
-    description:
-      "Designed an eye-catching UI, prioritizing to engage user experience. Utilized React to fetch and dynamically display cocktail data from an external API, ensuring comprehensive information for users.",
-    image: "/images/1.png",
-    tags: ["ReactJS"],
-    source: "https://github.com/bhattritik21/cocktails",
-    visit: "https://cocktail-details.netlify.app/",
-    id: 2,
-  },
-  {
-    title: "GameZone",
-    description:
-      " Its a Gaming Reviews Page.This project served as a valuable canvas for learning the basics of CSS, encompassing crucial concepts such as styling, layout, and responsiveness.",
-    image: "/images/5.png",
-    tags: ["Html", "css"],
-    source: "https://github.com/bhattritik21/Latest-PC-Games-Review",
-    visit: "https://bhattritik21.github.io/Latest-PC-Games-Review/",
-    id: 4,
-  },
-  {
-    title: "E-Shopping Cart",
-    description:
-      "Successfully navigated the challenges of state management within a dynamic shopping environment, showcasing my ability to handle complex data structures and application logic.",
-    image: "/images/2.png",
-    tags: ["ReactJS"],
-    source: "https://github.com/bhattritik21/shopping-cart",
-    visit: "https://iphoneshopping-cart.netlify.app/",
-    id: 5,
-  },
-  {
-    title: " Bhatt Blogs",
-    description: "Check out my own Blog",
-    image: "/images/projects.jpeg",
-    tags: ["Html", "css"],
-    source: "https://github.com/bhattritik21/Bhatt-blogs.github.io",
-    visit: "https://bhattritik21.github.io/Bhatt-blogs.github.io/",
-    id: 6,
-  },
+  }
 ];
 
-export const TimeLineData = [
-  { year: 2019, text: "Started my journey" },
-  { year: 2020, text: "Build my first ever front end Project" },
-  { year: 2022, text: "Started an Internship" },
-  {
-    year: 2022 - 23,
-    text: "Explpored the web dev world more, learened new frameworks and techs",
-  },
-  { year: 2023, text: "Joined HHAExchange as an Associate Software Engineer" },
-];
